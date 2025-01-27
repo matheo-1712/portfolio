5b2ac16454eb27648cf464f422a955d3b26a0aa8
matheo-prd <perod.matheo@gmail.com>
2025-01-27 23:11:22 +0100

    Next.js + Création d'un début de maquette de la première page de mon portfolio

---
+++ src/app/projet/page.tsx
@@ -0,0 +1,15 @@
+export default function Projet() {
+    return (
+        <div>
+            <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+                <h1 className="text-4xl font-bold">Projet</h1>
+                <p className="text-center text-lg">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                    tristique, nulla et aliquam lacinia, elit diam egestas augue, ac
+                    facilisis nisi nisl eu libero. Sed euismod, nisl at blandit
+                    consectetur, nunc nisi tincidunt nisl, in consequat nisi nisl eu          libero. Sed euismod, nisl at blandit consectetur, nunc nisi tincidunt nisl, in consequat nisi nisl eu libero. Sed euismod, nisl at blandit consectetur, nunc nisi tincidunt nisl, in consequat nisi nisl eu libero.
+                </p>
+            </main>
+        </div>
+    );
+}
