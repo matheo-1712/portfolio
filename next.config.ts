a6f3942b1b6d9df04f9ba8acadaf532f21f4065f
matheo-1712 <perod.matheo@gmail.com>
2026-01-16 16:12:53 +0100

    feat: mise à jour des métadonnées des projets et ajout du support des images distantes

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,15 @@ import remarkMdxFrontmatter from "remark-mdx-frontmatter";
 
 const nextConfig: NextConfig = {
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.discordapp.com',
+        pathname: '**',
+      },
+    ],
+  },
 };
 
 const withMDX = createMDX({
